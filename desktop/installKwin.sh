546036c2f001423a614b1e7da93e0411c5b82847
princenizroh <princenizroh7@gmail.com>
2024-01-12 06:40:21 +0800

    update script

--- desktop/installKwin.sh
+++ desktop/installKwin.sh
@@ -1,43 +1,71 @@
 #!/bin/bash
 dotfiles="$(pwd)/"
 
-echo "kwin from this config"
+{	
+	echo "This is Kwin setup"
+	echo -n "Do you want to skip the installation? [y/n]: "
+	read -r skip
 
-echo "Updating"
-sudo pacman -Syu --noconfirm
-echo "Installing dependencies"
-sudo pacman --noconfirm -S xorg xorg-xinit xterm plasma plasma-desktop plasma-wayland-session kde-applications kdeplasma-addons sddm
-sudo pacman --noconfirm -S kitty fish nvim zoxide bat ripgrep fd lazygit eza
+	if [ -z "$skip" ]; then
+		echo "Invalid input or empty choice. Existing."
+		exit 1
+	fi
+
+	if [ $skip != "y" ]; then
+		echo "Updating"
+		sudo pacman -Syu --noconfirm
+		echo "Installing dependencies"
+		sudo pacman --noconfirm -S xorg xorg-xinit xterm plasma plasma-desktop plasma-wayland-session kde-applications kdeplasma-addons sddm
+		sudo pacman --noconfirm -S kitty fish nvim zoxide bat ripgrep fd lazygit eza
+	fi
+} || { 
+	echo "Error installation" >> failures.log
+}
+
 {
-	echo -n "do you want symlink or copy [cp/ln/n]: "
-	read config
-	if [ $config == cp ]; then
-		cp -r ../.config/fish ~/.config/fish
-		cp -r ../.config/kitty ~/.config/kitty
+	echo "Will be moving configurations for fish and kitty"
+	echo -n "Do you want symlink or copy [cp/ln/n]: "
+	read -r config
+
+	if [ -z "$config" ]; then
+		echo "Invalid input or empty choice. Existing."
+		exit 1
+	fi
+
+	if [ "$config" == "cp" ]; then
+		echo "Copying configurations"
+		cp -r "$dotfiles../.config/fish" ~/.config/fish
+		cp -r "$dotfiles../.config/kitty" ~/.config/kitty
 	fi	
 
-	if [ $config == ln ]; then
+	if [ "$config" == "ln" ]; then
+		echo "Removing existing configurations"
 		rm -rf ~/.config/fish
 		rm -rf ~/.config/kitty
-		ln -s $dotfiles../.config/fish  ~/.config/	
-		ln -s $dotfiles../.config/kitty ~/.config/
+		
+		echo "Creating symlinks"
+		ln -s "$dotfiles../.config/fish"  ~/.config/	
+		ln -s "$dotfiles../.config/kitty" ~/.config/
 	fi
 }
-{
-	echo -e "do you want config \n1. lunarvim \n2. lazyvim \n3. nvim scratch"
-       	echo -n "Choose ('numb' / n): "
 
-	read vim 
-	if [ $vim == "1" ]; then
-		ln -s $dotfiles../.config/lvim ~/.config/
+{	
+	echo -e "Do you want additional configurations?"
+	echo -e "1. lunarvim\n2. lazyvim\n3. nvim scratch\nn. No additional configurations"
+       	echo -n "Choose ('numb'/n): "
+	read -r vim
+
+	if [ -z "$vim" ]; then
+		echo "Invalid input or empty choice. Exiting."
+		exit 1
 	fi
 
-	if [ $vim == "2" ]; then
-		ln -s $dotfiles/../.config/lavim ~/.config/
-	fi	
-	
-	if [ $vim == "3" ]; then
-		ln -s $dotfiles../.config/nvim ~/.config/
-	fi
+	case $vim in 
+		"1") echo "Creating symlink for lunarvim"; ln -s "$dotfiles../.config/lvim" ~/.config/ ;;
+		"2") echo "Creating symlink for lazyvim"; ln -s "$dotfiles../.config/lavim" ~/.config/ ;;
+		"3") echo "Creating symlink for nvim scratch"; ln -s "$dotfiles../.config/nvim" ~/.config/ ;;
+		"n") echo "No additional configurations. See ya" ;;
+		*) echo "Invalid option" ;;
+	esac
 }
 
--- script/installArchInstall.sh
+++ script/installArchInstall.sh
@@ -14,16 +14,21 @@ cat <<"EOF"
 EOF
 
 echo "Need installation for sudo!!"
-echo "this is installation package for arch installation"
-echo "Install arch package to root partition"
-echo -e "1. system \n2. desktop\n3. grub \n4. Utilities packages \n5. repo installer"
-echo -n "What do you want to install ('numb' / n): "
-read install
+echo "This is installation package for arch installation"
+echo -e "1. System \n2. Desktop\n3. Grub \n4. Utilities packages \n5. Repo installer"
+echo -n "What do you want to install? ['numb'/n]: "
+read -r install
 
-while [ $install != n ]; do 
-	echo ""
-	if  [[ "$install" == "1" ]]; then
+if [ -z "$install" ]; then
+	echo "Invalid input or empty choice. Exiting."
+	exit 1
+fi
+
+case $install in
+	1) # System Installation
+		echo "Install arch package to root partition"
 		echo "Before instalation update keyring"
+
 		pacman -Sy archlinux-keyring && pacman -Syyu
 		pacstrap -i /mnt base base-devel linux linux-lts linux-headers linux-firmware intel-ucode sudo nano vim git neofetch networkmanager dhcpcd pulseaudio bluez blueman bluez-utils github-cli
 		systemctl enable dhcpcd.service
@@ -33,78 +38,98 @@ while [ $install != n ]; do
 		systemctl --user enable pulseaudio
 		systemctl --user start pulseaudio
 
-	fi
+	;;
 
-	if  [[ "$install" == "2" ]]; then 
+	2) # Desktop installation
 		sudo pacman -S xorg xorg-xinit xterm plasma plasma-desktop plasma-wayland-session kde-applications kde-plasma-addons sddm 
 		sudo systemctl enable sddm.service
-	fi
-
-	if  [[ "$install" == "3" ]]; then
+	;;
+	
+	3) # Grub installation
 		pacman -S grub efibootmgr dosfstools mtools
-       		echo -n "do you want install os-prober for dual boot windows?[y/n] "
-		read os_prober
+       		echo -n "do you want install os-prober for dual boot windows? [y/n] "
+		read -r os_prober
 
-		if [[ "$os_prober" == "y" ]]; then
-	       		pacman -S os-prober
-			{
-				cp ../etc/default/grub /etc/default
-			} || {
-				echo "Error Moving files"
-			}
+		if [ -z "$os_prober" ]; then
+			echo "Invalid input or empty choice. Exiting."
+			exit 1
 		fi
+
+		if [ "$os_prober" == "y" ]; then
+	       		pacman -S os-prober
+		fi
+
+		{
+			install -Dm644 "$dotfiles/etc/default/grub" "/etc/default/grub"
+		} || {
+			echo "Error Moving files"
+		}
+
+
 		grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
 		grub-mkconfig -o /boot/grub/grub.cfg	
-	fi
+	;;
 
-	if  [[ "$install" == "4" ]]; then
+	4) # Utilities packages installation
 		echo "Updating"
 		sudo pacman -Syyu --noconfirm
 		echo "installing dependencies"
-		sudo pacman -S pipewire rsync ntfs-3g nvidia firefox brave-bin nvim unrar tar htop exfat-utils fuse-exfat zoxide wl-clipboard lazygit kitty kiten curl libreoffice-fresh gimp kdenlive audacity 
+		# install essential utilities
+		sudo pacman -S pipewire rsync ntfs-3g nvidia firefox unrar tar htop exfat-utils fuse-exfat zoxide wl-clipboard lazygit kitty kiten curl libreoffice-fresh gimp kdenlive audacity 
 		sudo pacman -S ufw
 		sudo ufw enable
 		sudo systemctl enable ufw.service 	
-	fi
-
-	if [[ "$install" == "5" ]]; then
+	;;
+	
+	5) # Repo installation
 		echo "Installer from repository github"
 		echo -e "1. KDE Rounded Corner \n2. auto-cpufreq \n3. yay"
-		echo -n "What do you want to install ('numb'/ n): "
-		read repository
+		echo -n "Choose the installation option: "
+		read -r repository
 
-		if [[ "$repository" == "1" ]]; then
-			git clone https://github.com/matinlotfali/KDE-Rounded-Corners
-			cd KDE-Rounded-Corners
-			mkdir build
-			cd build
-			cmake .. --install-prefix /usr
-			make
-			sudo make install
-
-			echo -n "Do you want logout to have it activated[y/n]?"
-			read log_out
-			if [[ "$log_out" == "y" ]]; then 
-				kwin --replace &
-			fi
+		if [ -z "$repository" ]; then
+			echo "Invalid input or empty choice. Exiting."
+			exit 1
 		fi
 
-		if [[ "$repository" == "2" ]]; then
-			git clone https://github.com/AdnanHodzic/auto-cpufreq.git
-			cd auto-cpufreq/
-			sudo ./auto-cpufreq-installer
-			sudo auto-cpufreq --install
-		fi
+		case $repository in
+			1) #KDE Rounded Corner installation
+				git clone https://github.com/matinlotfali/KDE-Rounded-Corners ~/Public/KDE-RoundedCorners
+				cd ~/Public/KDE-Rounded-Corners
+				mkdir build
+				cd build
+				cmake .. --install-prefix /usr
+				make
+				sudo make install
 
-		if [[ "$repository" == "3" ]]; then
-			git clone https://aur.archlinux.org/yay.git
-			cd yay
-			makepkg -si
-			yay --version
-		fi
-	fi
-	echo ""
-	echo -e "1. system \n2. desktop\n3. grub \n4. Utilities packages \n5. repo installer"
-    	echo -n "What do you want installing (numb / exit): "
-    	read install
-done
+				echo -n "Do you want to logout and log back in to activate the changes? [y/n]"
+				read -r log_out
+
+				if [ -z "$log_out" ]; then
+					echo "Invalid input or empty choice. Exiting."
+					exit 1
+				fi
+
+				if [ "$log_out" == "y" ]; then 
+					kwin --replace &
+				elif [ "$log_out" == "n" ]; then
+					echo "okey maybe later but just type in cli 'kwin --replace &'"
+				fi
+			;;
+
+			2) # auto-cpufreq installation
+				git clone https://github.com/AdnanHodzic/auto-cpufreq.git ~/Public/auto-cpufreq
+				cd ~/Public/auto-cpufreq
+				sudo ./auto-cpufreq-installer
+				sudo auto-cpufreq --install
+			;;
+			
+			3) # yay installation
+				git clone https://aur.archlinux.org/yay.git ~/Public/yay 
+				cd ~/Public/yay
+				makepkg -si
+				yay --version
+			;;
+		esac
+	
+esac
