e085d1846ab6985abb11eb8d8b482d7f9fb99154
princenizroh <princenizroh7@gmail.com>
2024-01-11 19:44:51 +0800

    add repository install

--- script/installArchInstall.sh
+++ script/installArchInstall.sh
@@ -4,11 +4,12 @@ echo "this is installation package for arch installation"
 echo "Before instalation update keyring"
 pacman -Sy archlinux-keyring && pacman -Syyu
 echo "Install arch package to root partition"
-echo -e "1. system \n2. desktop\n3. grub \n4. packages \n5. yay"
-echo -n "What do you want installing (numb / exit): "
+echo -e "1. system \n2. desktop\n3. grub \n4. Utilities packages \n5. yay \n6. repo installer"
+echo -n "What do you want to install (numb / exit): "
 read install
 
 while [ $install != exit ]; do 
+
 	if  [[ "$install" == "1" ]]; then
 		pacstrap -i /mnt base base-devel linux linux-lts linux-headers linux-firmware intel-ucode sudo nano vim git neofetch networkmanager dhcpcd pulseaudio bluez blueman bluez-utils
 		systemctl enable dhcpcd.service
@@ -32,14 +33,21 @@ while [ $install != exit ]; do
 
 		if [[ "$os_prober" == "y" ]]; then
 	       		pacman -S os-prober
-			cp ../etc/default/grub /etc/default
+			{
+				cp ../etc/default/grub /etc/default
+			} || {
+				echo "Error Moving files"
+			}
 		fi
-		grub-install --target=x86_64-efi --bootloader-id=grub_uefi --reacheck
+		grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
 		grub-mkconfig -o /boot/grub/grub.cfg	
 	fi
 
 	if  [[ "$install" == "4" ]]; then
-		sudo pacman -S pipewire rsync ntfs-3g nvidia firefox brave-bin nvim
+		sudo pacman -S pipewire rsync ntfs-3g nvidia firefox brave-bin nvim unrar tar htop exfat-utils fuse-exfat zoxide wl-clipboard lazygit kitty kiten curl libreoffice-fresh gimp kdenlive audacity 
+		sudo pacman -S ufw
+		sudo ufw enable
+		sudo systemctl enable ufw.service 	
 	fi
 
 	if [[ "$install" == "5" ]]; then
@@ -49,7 +57,36 @@ while [ $install != exit ]; do
 		yay --version
 	fi
 
-	echo -e "1. system \n2. desktop\n3. grub \n4. packages \n5. yay"
+	if [[ "$install" == "6" ]]; then
+		echo "Installer from repository github"
+		echo -e "1. KDE Rounded Corner \n2. auto-cpufreq"
+		echo -n "What do you want to install (numb)"
+		read repository
+
+		if [[ "$repository" == "1" ]]; then
+			git clone https://github.com/matinlotfali/KDE-Rounded-Corners
+			cd KDE-Rounded-Corners
+			mkdir build
+			cd build
+			cmake .. --install-prefix /usr
+			make
+			sudo make install
+
+			echo -n "Do you want logout to have it activated[y/n]?"
+			read log_out
+			if [[ "$log_out" == "y" ]]; then 
+				kwin --replace &
+			fi
+		fi
+
+		if [[ "$repository" == "2" ]]; then
+			git clone https://github.com/AdnanHodzic/auto-cpufreq.git
+			cd auto-cpufreq/
+			sudo ./auto-cpufreq-installer
+			sudo auto-cpufreq --install
+		fi
+	fi
+	echo -e "1. system \n2. desktop\n3. grub \n4. Utilities packages \n5. yay \n6. repo installer"
     	echo -n "What do you want installing (numb / exit): "
     	read install
 done
