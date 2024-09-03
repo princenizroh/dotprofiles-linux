252e95895ef202f044f0a6e5e6da31359a9ab792
princenizroh <113965138+princenizroh@users.noreply.github.com>
2024-09-03 23:57:47 +0800

    Update nvim-ts-autotag.lua

--- .config/nvim/lua/princenizroh/plugins/nvim-ts-autotag.lua
+++ .config/nvim/lua/princenizroh/plugins/nvim-ts-autotag.lua
@@ -1,4 +1,8 @@
 return {
 	"windwp/nvim-ts-autotag",
 	lazy = false,
+	dependencies = { "nvim-treesitter/nvim-treesitter" }, -- pastikan Treesitter diatur sebagai dependensi
+	config = function()
+		require('nvim-ts-autotag').setup()
+	end,
 }
